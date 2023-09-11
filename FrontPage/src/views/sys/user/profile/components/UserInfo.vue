d25d14ef9ece24f7c465d79a8b295ef9d034c36b
郭志明 <zhiming.guo@zingbiz.com>
2023-09-11 19:46:00 +0800

    最终版本1

--- FrontPage/src/views/sys/user/profile/components/UserInfo.vue
+++ FrontPage/src/views/sys/user/profile/components/UserInfo.vue
@@ -2,7 +2,7 @@
  * @Author: 郭志明 zhiming.guo@zingbiz.com
  * @Date: 2023-05-12 17:11:38
  * @LastEditors: 郭志明 zhiming.guo@zingbiz.com
- * @LastEditTime: 2023-08-24 17:55:50
+ * @LastEditTime: 2023-09-11 19:45:48
  * @FilePath: /gzm_house/FrontPage/src/views/sys/user/profile/components/UserInfo.vue
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
 -->
